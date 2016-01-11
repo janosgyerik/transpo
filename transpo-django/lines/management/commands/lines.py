32498adad94dcd8b7bea653326a0a791dedf4e7c
Janos Gyerik <janos@kronos>
2016-01-11 22:36:02 +0100

    added lines command

---
+++ transpo-django/lines/management/commands/lines.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from lines import models
+
+
+class Command(BaseCommand):
+    help = 'Manage lines'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--create', metavar='name')
+
+    def handle(self, *args, **options):
+        if options['create']:
+            line = models.Line.objects.create(name=options['create'])
+            self.print_line(line)
+        else:
+            for line in models.Line.objects.all():
+                self.print_line(line)
+
+    def print_line(self, line):
+        self.stdout.write('{}'.format(line))
