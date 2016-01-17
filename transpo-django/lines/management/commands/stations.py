96f3cd356ecfe25242f1e9798f39688afdb036a4
Janos Gyerik <janos@kronos>
2016-01-17 19:36:41 +0100

    moved station commands to methods

--- transpo-django/lines/management/commands/stations.py
+++ transpo-django/lines/management/commands/stations.py
@@ -12,26 +12,29 @@ class Command(BaseCommand):
                             help='Show times of stations')
 
     def handle(self, *args, **options):
-        line_name_option = options['create']
-        if line_name_option:
-            try:
-                line_name, station_name = options['create'].split(':')
-            except ValueError as e:
-                raise CommandError('could not parse line:name format: {}\n  {}'.format(line_name_option, e))
-
-            try:
-                line = models.Line.objects.get(name=line_name)
-            except models.Line.DoesNotExist:
-                raise CommandError('No Line with this name: {}'.format(line_name))
-
-            station = models.Station.objects.create(name=station_name, line=line)
-            self.print_station(station)
-
+        if options['create']:
+            self.create_station(options['create'])
         else:
-            show_times = options['times']
+            self.list_stations(options)
 
-            for station in models.Station.objects.all():
-                self.print_station(station, show_times)
+    def create_station(self, line_name_option):
+        try:
+            line_name, station_name = line_name_option.split(':')
+        except ValueError as e:
+            raise CommandError('could not parse line:name format: {}\n  {}'.format(line_name_option, e))
+
+        try:
+            line = models.Line.objects.get(name=line_name)
+        except models.Line.DoesNotExist:
+            raise CommandError('No Line with this name: {}'.format(line_name))
+
+        station = models.Station.objects.create(name=station_name, line=line)
+        self.print_station(station)
+
+    def list_stations(self, options):
+        show_times = options['times']
+        for station in models.Station.objects.all():
+            self.print_station(station, show_times)
 
     def print_station(self, station, show_times=False):
         self.stdout.write('{}: {}'.format(station.id, station))
