547456081c103f67eb0e302ff2902384aae99447
Janos Gyerik <janos@kronos>
2016-01-22 23:06:54 +0100

    removed pointless dummy data test

--- transpo-django/lines/tests.py
+++ transpo-django/lines/tests.py
@@ -43,11 +43,6 @@ class DailyTimesTestCase(TestCase):
         station.register_daily_times(days=[DailySchedule.WEEKENDS], times=self.weekend_times)
         station.register_daily_times(days=[DailySchedule.SATURDAY], times=self.saturday_times)
 
-        dummy_line = Line.objects.create(name='dummy line')
-        dummy_station = Station.objects.create(line=dummy_line, name='dummy station')
-        dummy_station.register_daily_times(days=[DailySchedule.WEEKDAYS], times=self.weekday_times)
-        dummy_station.register_daily_times(days=[DailySchedule.SUNDAY], times=self.saturday_times)
-
     def test_mon_finds_weekdays(self):
         self.assertEquals(self.weekday_times, self._times(self.station.daily_times(self.next_weekday(0))))
 
